6c87cd1fba2f2491730721fb2da7e1e780d8fde9
pieri1211 <mariapierina12@hotmail.com>
2026-05-12 13:26:30 -0500

    Initial commit

---
+++ src/main/java/com/empresa/pedidos/PedidosIntegradoApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidosIntegradoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PedidosIntegradoApplication.class, args);
+    }
+
+}
